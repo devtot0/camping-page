b5e2576171e274faa1d579dd31a111c43d3b052c
User <lifesim11@gmail.com>
2024-01-19 15:09:12 +0100

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,77 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "services", label: "Usługi" },
+  { href: "/", key: "contact_us", label: "Kontakt" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Malownicza lokalizacja",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Pięknie położenie nad rzeką w pobliżu lasu zapewnia szerokie pole do hobbystycznych aktywności oraz cieszące oko widoki.",
+  },
+  {
+    title: "Wysoka dostępność miejsc",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Duża powierzchnia naszego campingu sprawia, że jak dotąd nigdy nie odmówiliśmy żadnego klientowi.",
+  },
+  {
+    title: "Dostęp do mediów",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "Na terenie campingu znajdują się wszytskie niezbędne media: prąd, woda, gaz.",
+  },
+  {
+    title: "Korzystna lokalizacja",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Camping położony jest zaledwie 50km od Warszawy, przez co nie musisz tracić czasu na długą podróż by znaleźć się blisko pięknej natury.",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      "About Hilink",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
+    ],
+  },
+  {
+    title: "Our Community",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Kontakt",
+  links: [
+    { label: "Recepcja", value: "123-456-7890" },
+    { label: "Email", value: "recepcja@kampinos.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Znajdź nas na",
+  links: ["/facebook.svg", "/instagram.svg", "/youtube.svg"],
+};
